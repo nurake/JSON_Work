6bc05c21270cb12e70f226850b99f165928f354b
nurake <nurake98@gmail.com>
2021-11-28 15:34:50 +0600

    Initial Commit

---
+++ JSON_Work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON_Work
+//
+//  Created by nurake on 28.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
